ac72fc0f994fcd677c0d080bef32e9ca6a4bad2b
Anastasia <Anastasia@gmail.com>
2024-09-12 15:10:45 +0300

    Add project files.

---
+++ ConsoleApplication6/ConsoleApplication6.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int arr[5]{ 10,5,3,6,20 };
+	cout << arr[0] << endl; //10
+	cout << &arr[0] << endl; //adress
+	cout << arr << endl; //adress
+
+	int* ptr = arr;
+	cout << ptr << endl;
+	cout << *ptr << endl; //10
+	ptr++;
+	cout << ptr << endl;
+	cout << *ptr << endl; //5
+	ptr = ptr + 3; //ptr += 3 
+	cout << *ptr << endl; //20
+}
